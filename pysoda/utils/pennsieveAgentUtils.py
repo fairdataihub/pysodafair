ea358974601da876f423440391fbd3ff85d21968
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-12-12 10:40:11 -0800

    feat: connect_pennsieve_client client error handling updated

--- pysoda/utils/pennsieveAgentUtils.py
+++ pysoda/utils/pennsieveAgentUtils.py
@@ -12,7 +12,9 @@ def connect_pennsieve_client(account_name):
             raise PennsieveAgentRPCError(f"Could not connect to the Pennsieve Agent error coming from the Agent. This can likely be resolved by retrying. If the issue persists, please contact the SODA team and they will reach out to Pennsieve to help resolve this.") from e
         elif isinstance(e, AttributeError):
             raise Exception("The Pennsieve Agent cannot access datasets but needs to in order to work. Please try again. If the issue persists, please contact the SODA team. The SODA team will contact Pennsieve to help resolve this issue.") from e
-        elif "403" or "404" in str(e):
-            raise PennsieveAccountInvalid("Please select a valid Pennsieve account.") from e
+        elif "Profile not found" in str(e):
+            raise PennsieveAccountInvalid(account_name) from e
         elif "Error connecting to server" in str(e):
             raise PennsieveAgentError(f"Could not connect to the Pennsieve agent: {e}") from e
+        else:
+            raise PennsieveAgentError("An unknown error occurred when trying to connect to the Pennsieve Agent. If this issue persists, please contact the SODA team.") from e
